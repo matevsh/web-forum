9e69ab446896d07c219d0a755982798cf06d7252
Mateusz Marcinkowski <mateuszmarcinkowski44@gmail.com>
2022-10-21 07:19:57 +0200

    change "any" type in thread loader

--- packages/frontend/src/loaders/thread.loader.ts
+++ packages/frontend/src/loaders/thread.loader.ts
@@ -1,9 +1,10 @@
 import axios, {AxiosResponse} from 'axios';
 import {thread} from '../../types/thread';
+import {LoaderFunctionArgs} from 'react-router-dom';
 
 const URL = 'http://localhost:3000/api/data';
 
-const threadLoader = async ({params}: any) => {
+const threadLoader = async ({params}: LoaderFunctionArgs) => {
     const response: AxiosResponse<thread> = await axios.get(`${URL}/${params.threadId}`);
     return response.data;
 };
