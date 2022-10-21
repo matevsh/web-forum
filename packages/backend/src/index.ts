1ad87c7c3c1e071b50bbfc5a9a9ad66f5d0d23a7
Mateusz Marcinkowski <mateuszmarcinkowski44@gmail.com>
2022-10-21 14:12:01 +0200

    express sessions, cookie setup, prepare to implement auth.

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -3,12 +3,25 @@ import dotenv from 'dotenv';
 import apiRouter from './shared/api.router';
 import cors from 'cors';
 import error404 from './shared/error/404';
-
-const app = express();
-
-app.use(cors({ origin: '*' }));
+import cookieParser from 'cookie-parser';
+import sessions from 'express-session';
 
 dotenv.config();
+
+const app = express();
+app.use(cors({ origin: '*' }));
+
+app.use(cookieParser());
+
+const cookieMonth = 1000* 60* 60* 24* 30;
+const {SECRET} = process.env;
+const cookieOptions = {
+    secret: `${SECRET}`,
+    saveUninitialized: true,
+    cookie: { maxAge: cookieMonth },
+    resave: false,
+};
+app.use(sessions(cookieOptions));
 
 app.use('/api', apiRouter);
 app.get('*', error404);
--- packages/backend/src/shared/api.router.ts
+++ packages/backend/src/shared/api.router.ts
@@ -1,8 +1,10 @@
 import {Router} from 'express';
 import providerRouter from '../provider/router';
+import authRouter from '../auth/router';
 
 const apiRouter = Router();
 
 apiRouter.use('/data', providerRouter);
+apiRouter.use('/auth', authRouter);
 
 export default apiRouter;
